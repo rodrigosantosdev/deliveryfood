60093d5bbde8986654bee56b44fdd7cb6d2a050a
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-10-11 17:02:50 -0300

    feat: adicionados componentes na pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HeadlineCard from "./components/HeadlineCard";
+import Food from "./components/Food";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <HeadlineCard />
+      <Food />
     </div>
   );
 }
