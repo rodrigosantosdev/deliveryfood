9377de9d98c8c7d034506f4956a8021650812744
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-10-11 17:00:49 -0300

    feat: mock de dados

---
+++ src/data/index.js
@@ -0,0 +1,125 @@
+export const product = [
+  {
+    id: 1,
+    name: "Combo Chesseburguer + Batata",
+    category: "Burguer",
+    image:
+      "https://images.pexels.com/photos/3616956/pexels-photo-3616956.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 2,
+    name: "Bacon Chesseburguer",
+    category: "Burguer",
+    image:
+      "https://images.pexels.com/photos/3764353/pexels-photo-3764353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 3,
+    name: "Combo Burguer",
+    category: "Burguer",
+    image:
+      "https://images.pexels.com/photos/3023476/pexels-photo-3023476.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 4,
+    name: "Especial Burguer",
+    category: "Burguer",
+    image:
+      "https://images.pexels.com/photos/1893557/pexels-photo-1893557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 5,
+    name: "Mega Cheedar Burguer",
+    category: "Burguer",
+    image:
+      "https://images.pexels.com/photos/3738762/pexels-photo-3738762.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 6,
+    name: "Vegano Burguer",
+    category: "Burguer",
+    image:
+      "https://images.pexels.com/photos/572981/pexels-photo-572981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 7,
+    name: "Pizza Fatia",
+    category: "Pizza",
+    image:
+      "https://images.pexels.com/photos/708587/pexels-photo-708587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 8,
+    name: "Vegana",
+    category: "Pizza",
+    image:
+      "https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 9,
+    name: "Combo 3 Pizza",
+    category: "Pizza",
+    image:
+      "https://images.pexels.com/photos/1566837/pexels-photo-1566837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 10,
+    name: "Pepsi Lata",
+    category: "Bebidas",
+    image:
+      "https://images.pexels.com/photos/1292294/pexels-photo-1292294.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 11,
+    name: "Coca Cola Lata",
+    category: "Bebidas",
+    image:
+      "https://images.pexels.com/photos/9706990/pexels-photo-9706990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+  {
+    id: 12,
+    name: "Batata Frita",
+    category: "Complementos",
+    image:
+      "https://images.pexels.com/photos/1301373/pexels-photo-1301373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    price: "$ 999",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    name: "Burguer",
+    image:
+      "https://images.pexels.com/photos/572981/pexels-photo-572981.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 2,
+    name: "Pizza",
+    image:
+      "https://images.pexels.com/photos/1146760/pexels-photo-1146760.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 3,
+    name: "Bebidas",
+    image:
+      "https://images.pexels.com/photos/9706990/pexels-photo-9706990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+  {
+    id: 4,
+    name: "Complementos",
+    image:
+      "https://images.pexels.com/photos/1301373/pexels-photo-1301373.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
